8a398c1669fccc3d0dc5193bfcd52d6ca702dded
higormartinsb <136651293+higormartinsb@users.noreply.github.com>
2025-03-23 16:12:43 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -72,7 +72,7 @@ window.onscroll = function() {
   //PWA
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sandubalho/service-worker.js')
+      navigator.serviceWorker.register('/service-worker.js')
         .then((registration) => {
           console.log('Service Worker registrado com sucesso:', registration);
         })
